9fca306a262bd889ce49210c293c16c99dddd3ba
Tsering Dhondup <tyddhondup88@gmail.com>
2024-01-29 23:51:53 -0500

    getApplicationDetails branch created

--- src/pages/ApplicationDetailPage/ApplicationDetailPage.js
+++ src/pages/ApplicationDetailPage/ApplicationDetailPage.js
@@ -1,5 +1,2 @@
-const ApplicatioDetailPage = () => {
-    return <h1>Application Detail Page</h1>;
-  };
-  export default ApplicatioDetailPage;
-  
+const ApplicatioDetailPage = () => {};
+export default ApplicatioDetailPage;
