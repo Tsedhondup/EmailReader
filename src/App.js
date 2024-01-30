cc4ae8712e199fb149a007ec77a72cbcb13400e5
Tsering Dhondup <tyddhondup88@gmail.com>
2024-01-29 21:10:08 -0500

    routes for the pages were created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
+// COMPONENTS
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import ApplicationListsPage from "./pages/ApplicationListsPage/ApplicationListsPage";
+import ApplicationDetailPage from "./pages/ApplicationDetailPage/ApplicationDetailPage";
+import EmailDetailPage from "./pages/EmailDetailPage/EmailDetailPage";
+import ScheduleInterviewPage from "./pages/ScheduleInterviewPage/ScheduleInterviewPage";
+import InterviewListsPage from "./pages/InterviewListsPage/InterviewListsPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
+import ContactPage from "./pages/ContactPage/ContactPage";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<WareHouse />} />
-          <Route path="/warehouses/:id" element={<WareHouseDetailPage />} /> */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/applicationLists" element={<ApplicationListsPage />} />
+          <Route
+            path="/applicationDetail/:id"
+            element={<ApplicationDetailPage />}
+          />
+          <Route path="/emailDetail/:id" element={<EmailDetailPage />} />
+          <Route
+            path="/scheduleInterview"
+            element={<ScheduleInterviewPage />}
+          />
+          <Route path="/interviewLists" element={<InterviewListsPage />} />
+          <Route path="/Login" element={<LoginPage />} />
+          <Route path="/SignUp" element={<SignUpPage />} />
+          <Route path="/About" element={<AboutPage />} />
+          <Route path="/Contact" element={<ContactPage />} />
         </Routes>
       </BrowserRouter>
     </div>
