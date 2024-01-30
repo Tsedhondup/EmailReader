cbf55c5433e9b1f88ac050168fbda2004a62aaca
Tsering Dhondup <tyddhondup88@gmail.com>
2024-01-29 20:16:02 -0500

    React-router-dom installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<WareHouse />} />
+          <Route path="/warehouses/:id" element={<WareHouseDetailPage />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
