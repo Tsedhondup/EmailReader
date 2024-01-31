a083479263bf0e6aab98b24a8891cd9d14010270
Tsering Dhondup <tyddhondup88@gmail.com>
2024-01-31 00:22:58 -0500

    Getting all application lists and current application achieved

--- src/pages/ApplicationDetailPage/ApplicationDetailPage.js
+++ src/pages/ApplicationDetailPage/ApplicationDetailPage.js
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "./ApplicationDetailPage.scss";
 import ApplicationListsSidebar from "../../components/ApplicationListsSidebar/ApplicationListsSidebar";
+import axios from "axios";
 
 const ApplicatioDetailPage = () => {
+  const { id } = useParams();
+  const BASE_URL = "http://localhost:8080/";
+
+  const [currentApplication, setCurrentApplicaton] = useState(" ");
+  const [applicationLists, setApplicationLists] = useState([]);
+  const [emails, setEmails] = useState([]);
+  const [interviews, setInterviews] = useState([]);
+  const [hasLoaded, setHasLoaded] = useState(false);
+  console.log(currentApplication);
+  const getApplicationDetails = () => {
+    axios
+      .get(`${BASE_URL}getApplicationDetails/${id}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+  const getAllApplications = () => {
+    axios
+      .get(`${BASE_URL}getAllApplications`)
+      .then((response) => {
+        setApplicationLists(response.data);
+        console.log(response.data);
+        return response.data;
+      })
+      .then((applications) => {
+        applications.forEach((item) => {
+          if (`${item.id}` === id) {
+            setCurrentApplicaton(item);
+          }
+        });
+      })
+      .then(() => {
+        setHasLoaded(true);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+  useEffect(() => {
+    getApplicationDetails();
+    getAllApplications();
+  }, []);
   return (
     <article className="application-detail">
       <ApplicationListsSidebar />
