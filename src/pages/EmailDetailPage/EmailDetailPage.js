5224506831446dcb9ae4447743ed32ede7fb9302
Tsering Dhondup <tyddhondup88@gmail.com>
2024-01-31 22:29:56 -0500

    Fetching data from API completed

--- src/pages/EmailDetailPage/EmailDetailPage.js
+++ src/pages/EmailDetailPage/EmailDetailPage.js
@@ -1,4 +1,20 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 const EmailDetailPage = () => {
+  const { id } = useParams();
+  const BASE_URL = "http://localhost:8080/";
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}emailDetail/${id}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return <h1>Email detail Page</h1>;
 };
 export default EmailDetailPage;
