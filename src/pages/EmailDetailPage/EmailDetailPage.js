cb21491dc0a4f5218d661cbdde7056880e9fd2d9
Tsering Dhondup <tyddhondup88@gmail.com>
2024-02-01 15:34:27 -0500

    getting and rendering email detail was completed

--- src/pages/EmailDetailPage/EmailDetailPage.js
+++ src/pages/EmailDetailPage/EmailDetailPage.js
@@ -6,12 +6,15 @@ const EmailDetailPage = () => {
   const { id } = useParams();
   const BASE_URL = "http://localhost:8080/";
   const [emailURL, setEmailURL] = useState("");
+  const [emailData, setEmailData] = useState("");
   const [hasLoaded, setHasLoaded] = useState(false);
   useEffect(() => {
     axios
       .get(`${BASE_URL}emailDetail/${id}`)
       .then((response) => {
         setEmailURL(response.data.link_to_email_page);
+        setEmailData(response.data);
+        console.log(response.data);
       })
       .then(() => {
         setHasLoaded(true);
@@ -28,7 +31,27 @@ const EmailDetailPage = () => {
     );
   }
   if (hasLoaded) {
-    return <iframe src={emailURL} title="Fetched email detail"></iframe>;
+    return (
+      <article>
+        <section>
+          <div>
+            <h2>{emailData.subject}</h2>
+          </div>
+          <div>
+            <h2>{emailData.company_name}</h2>
+            <h3>Position : {emailData.position}</h3>
+          </div>
+        </section>
+        <section>
+          <iframe src={emailURL} title="Fetched email detail"></iframe>;
+        </section>
+        <section>
+          <button>Schedule Interview</button>
+          <button>Add to favourite</button>
+          <button>Delete</button>
+        </section>
+      </article>
+    );
   }
 };
 export default EmailDetailPage;
