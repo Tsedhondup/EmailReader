05eb4141ff3eed7fae7ff93974899dfa5140a0bb
Tsering Dhondup <tyddhondup88@gmail.com>
2024-02-01 23:52:35 -0500

    form element added to addApplication page

--- src/pages/AddApplication/AddApplication.js
+++ src/pages/AddApplication/AddApplication.js
@@ -1,4 +1,26 @@
 const AddApplication = () => {
-  return <h1>Add application </h1>;
+  return (
+    <>
+      <h1>Add job application</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <label>Company Name</label>
+        <input name="company-name" placeholder="company name"></input>
+        <label>Company Name</label>
+        <input name="position" placeholder="position"></input>
+        <label>Company email</label>
+        <input name="contact-email" placeholder="company email"></input>
+        <label>Contact Number</label>
+        <input name="contact-phone" placeholder=" contact number"></input>
+        <section>
+          <button>Add</button>
+          <button>Cancel</button>
+        </section>
+      </form>
+    </>
+  );
 };
 export default AddApplication;
