fc01c12dd5efcbd4e546907eb3cd1ea1243352dd
Tsering Dhondup <tyddhondup88@gmail.com>
2024-02-02 13:08:10 -0500

    redirecting to application lists page fixed

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 import { useState } from "react";
+import { redirect, useNavigate } from "react-router-dom";
 const LoginPage = () => {
   const API_BASE_URL = "http://localhost:8080/";
 
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
+
+  const navigate = useNavigate();
 
   const handlUserName = (event) => {
     setUserName(event.target.value);
@@ -20,39 +24,55 @@ const LoginPage = () => {
       })
       .then((response) => {
         console.log(response.data);
+        sessionStorage.setItem("userId", response.data.userId);
+      })
+      .then(() => {
+        setIsLogin(true);
+      })
+      .then(() => {
+        navigate("/applicationLists");
       })
       .catch((err) => {
         console.log(err.message);
       });
   };
-  return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
-    >
-      <h2>Enter credentials</h2> <label>Username</label>
-      <input
-        name="name"
-        placeholder="enter username"
-        value={userName}
-        type="name"
-        onChange={(event) => {
-          handlUserName(event);
+  if (!isLogin) {
+    return (
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
         }}
-      ></input>
-      <label>Email</label>
-      <input
-        name="email"
-        placeholder="enter email"
-        value={email}
-        type="email"
-        onChange={(event) => {
-          handlEmail(event);
-        }}
-      ></input>
-      <button onClick={handleLogin}>Login</button>
-    </form>
-  );
+      >
+        <h2>Enter credentials</h2> <label>Username</label>
+        <input
+          name="name"
+          placeholder="enter username"
+          value={userName}
+          type="name"
+          onChange={(event) => {
+            handlUserName(event);
+          }}
+        ></input>
+        <label>Email</label>
+        <input
+          name="email"
+          placeholder="enter email"
+          value={email}
+          type="email"
+          onChange={(event) => {
+            handlEmail(event);
+          }}
+        ></input>
+        <button
+          onClick={() => {
+            handleLogin();
+          }}
+        >
+          Login
+        </button>
+      </form>
+    );
+  }
+  // if (isLogin) return redirect("/applicationLists");
 };
 export default LoginPage;
