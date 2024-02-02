4fad00ebecc54f12232cd53185cfbf943c6a1112
Tsering Dhondup <tyddhondup88@gmail.com>
2024-02-02 14:59:59 -0500

    dynamic header component created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,37 @@
+import axios from "axios";
 import "./Header.scss";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 const Header = () => {
-  return (
-    <section className="header">
-      <div className="header__content">
+  const API_BASE_URL = "http://localhost:8080/";
+  let location = useLocation();
+  const [user, setUser] = useState("");
+  const [hasUser, setHasUser] = useState(false);
+
+  useEffect(() => {
+    if (sessionStorage.getItem("userId")) {
+      axios
+        .get(`${API_BASE_URL}profile`, {
+          headers: {
+            userId: sessionStorage.getItem("userId"),
+          },
+        })
+        .then((response) => {
+          setUser(response.data[0]);
+        })
+        .then(() => {
+          setHasUser(true);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
+    // setPageLocation(location.pathname);
+  }, [location.pathname]);
+
+  if (!hasUser) {
+    return (
+      <section className="header">
         <div>
           <Link className="header-logo-container">
             <h2 className="header-logo-container__logo">
@@ -11,13 +39,38 @@ const Header = () => {
             </h2>
           </Link>
         </div>
-        <div className="header-button-container">
-          <button className="header-button-container__button">
-            Add Application
-          </button>
+      </section>
+    );
+  }
+  if (hasUser) {
+    return (
+      <section className="header">
+        <div className="header__content">
+          <div>
+            <Link className="header-logo-container">
+              <h2 className="header-logo-container__logo">
+                Job Application Tracker
+              </h2>
+            </Link>
+          </div>
+          <div className="header-button-container">
+            <Link to="addApplication">
+              <button className="header-button-container__button">
+                Add Application
+              </button>
+            </Link>
+
+            <p>{user.full_name}</p>
+            <Link
+              to="/interviewLists"
+              className="header-button-container__button"
+            >
+              Interviews
+            </Link>
+          </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 };
 export default Header;
