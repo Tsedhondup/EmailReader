38e9d7f2a165f2d505770e3510ff0426dc38733c
Tsering Dhondup <tyddhondup88@gmail.com>
2024-02-02 10:48:00 -0500

    form elements added to login form

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,4 +1,13 @@
 const LoginPage = () => {
-  return <h1>Login Page</h1>;
+  const LoginPage = () => {
+    return (
+      <form>
+        <h2>Sign In</h2> <label>Username</label>
+        <input name="name" placeholder="enter username"></input>
+        <label>Email</label>
+        <input name="email" placeholder="enter email"></input>
+      </form>
+    );
+  };
 };
 export default LoginPage;
