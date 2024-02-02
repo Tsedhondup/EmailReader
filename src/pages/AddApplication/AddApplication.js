18d006a700030f5434412689439a1244569b30f6
Tsering Dhondup <tyddhondup88@gmail.com>
2024-02-02 10:45:39 -0500

    Form element added to addApplication page

--- src/pages/AddApplication/AddApplication.js
+++ src/pages/AddApplication/AddApplication.js
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import axios from "axios";
 const AddApplication = () => {
+  const API_BASE_URL = "http://localhost:8080/";
+  const [companyName, setCompanyName] = useState("");
+  const [position, setPosition] = useState("");
+  const [email, setEmail] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+
   return (
     <>
       <h1>Add job application</h1>
@@ -8,13 +16,33 @@ const AddApplication = () => {
         }}
       >
         <label>Company Name</label>
-        <input name="company-name" placeholder="company name"></input>
+        <input
+          name="company-name"
+          placeholder="company name"
+          value={companyName}
+          onChange={() => {}}
+        ></input>
         <label>Company Name</label>
-        <input name="position" placeholder="position"></input>
+        <input
+          name="position"
+          placeholder="position"
+          value={position}
+          onChange={() => {}}
+        ></input>
         <label>Company email</label>
-        <input name="contact-email" placeholder="company email"></input>
+        <input
+          name="contact-email"
+          placeholder="company email"
+          value={email}
+          onChange={() => {}}
+        ></input>
         <label>Contact Number</label>
-        <input name="contact-phone" placeholder=" contact number"></input>
+        <input
+          name="contact-phone"
+          placeholder="contact number"
+          value={contactNumber}
+          onChange={() => {}}
+        ></input>
         <section>
           <button>Add</button>
           <button>Cancel</button>
