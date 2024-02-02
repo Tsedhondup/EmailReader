2211b45cde8d70b223966314d642ae4dd764299f
Tsering Dhondup <tyddhondup88@gmail.com>
2024-02-01 20:55:14 -0500

    InterviewListsPage.scss file created

--- src/pages/InterviewListsPage/InterviewListsPage.js
+++ src/pages/InterviewListsPage/InterviewListsPage.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "./InterviewListsPage.scss";
 const InterviewListPage = () => {
   const API_BASE_URL = "http://localhost:8080/";
 
-  const [applications, setApplications] = useState("");
-  const [interviewLists, setInterviewLists] = useState("");
+  const [applications, setApplications] = useState([]);
+  const [interviewLists, setInterviewLists] = useState([]);
   const [hasLoaded, setHasloaded] = useState(false);
 
   const getAllApplications = () => {
@@ -12,7 +13,6 @@ const InterviewListPage = () => {
       .get(`${API_BASE_URL}getAllApplications`)
       .then((response) => {
         setApplications(response.data);
-        console.log(response.data);
       })
       .catch((err) => {
         console.log(err.message);
@@ -36,6 +36,43 @@ const InterviewListPage = () => {
     getAllApplications();
     getAllInterviews();
   }, []);
-  return <h1>Interview Lists Page</h1>;
+
+  if (hasLoaded) {
+    return (
+      <>
+        <article>
+          {applications.map((item) => {
+            return <p key={item.id}>{item.company_name}</p>;
+          })}
+        </article>
+        <article>
+          <section>
+            <h3>Company</h3>
+            <h3>About</h3>
+            <h3>Date</h3>
+            <h3>Reschedule</h3>
+            <h3>Status</h3>
+          </section>
+          <section className="interview-lists">
+            {interviewLists.map((item) => {
+              return (
+                <div key={item.id} className="interview-lists__interview">
+                  <p>{item.company_name}</p>
+                  <p>{item.about}</p>
+                  <p>{item.iterview_date}</p>
+                  <button id={item.id}>Change</button>
+                  <p>{item.status}</p>
+                </div>
+              );
+            })}
+          </section>
+        </article>
+      </>
+    );
+  }
+
+  if (!hasLoaded) {
+    return <h1>Loading.....</h1>;
+  }
 };
 export default InterviewListPage;
