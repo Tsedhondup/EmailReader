c8b15f7c40b882dc30d708f18e8230db91187b59
Tsering Dhondup <tyddhondup88@gmail.com>
2024-05-04 13:08:57 -0400

    Link react hook imported

--- src/pages/InterviewListsPage/InterviewListsPage.js
+++ src/pages/InterviewListsPage/InterviewListsPage.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import FollowUpEmailPage from "../FollowUpEmailPage/FollowUpEmailPage";
 import axios from "axios";
