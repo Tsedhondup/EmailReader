8ab89a6b8152103c4decbdf9cb1e9d5caab694d6
Tsering Dhondup <tyddhondup88@gmail.com>
2024-05-05 17:46:34 -0400

    state variables for email structure declared

--- src/pages/FollowUpEmailPage/FollowUpEmailPage.js
+++ src/pages/FollowUpEmailPage/FollowUpEmailPage.js
@@ -1,4 +1,27 @@
+import { useState } from "react";
+import axios from "axios";
 const FollowUpEmailPage = () => {
+  const API_BASE_URL = "http://localhost:8080/";
+
+  const [recipient, setRecipient] = useState();
+  const [subject, setSubject] = useState();
+  const [sender, setSender] = useState();
+  const [body, setBody] = useState();
+  const getAllApplications = () => {
+    axios
+      .post(`${API_BASE_URL}sendEmail/${sessionStorage.getItem("profileId")}`, {
+        headers: {
+          authorization: `Bearer ${sessionStorage.getItem("authToken")}`,
+        },
+      })
+      .then((response) => {
+        // setApplications(response.data);
+      })
+      .catch((err) => {
+        // navigate("/Login");
+        console.log(err.message);
+      });
+  };
   return (
     <>
       <h2>New Message</h2>
